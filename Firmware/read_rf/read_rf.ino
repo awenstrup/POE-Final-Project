05fd162c90e3e6b093a26ccce7553e93b77c3780
Alex Wenstrup <awenstrup@olin.edu>
2019-10-24 15:46:30 -0400

    Wrote code to drive sketch model car; have not tested yet; does not steer

--- Firmware/read_rf/read_rf.ino
+++ Firmware/read_rf/read_rf.ino
@@ -2,16 +2,24 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
+#include <Servo.h>
 
 //*************Define constants and global variables***************
-#define throttlePin 0
-#define steeringPin 0
+#define throttlePin 5 //column 2 on reciever side; left stick up/down
+#define steeringPin 6 //column 0 on reciever; right stick left/right
 
-#define rawThrottleMin 0
-#define rawThrottleMax 1000
+#define rawThrottleMin 1054
+#define rawThrottleMax 1888
+#define rawThrottleDeadZone 30
 
-#define rawSteeringMin 0
-#define rawSteeringMax 1000
+#define rawSteeringMin 1052
+#define rawSteeringMax 1886
+
+#define throttleOutMin 0
+#define throttleOutMax 100
+
+#define steeringOutMin 0
+#define steeringOutMax 0
 
 unsigned long rawThrottle;
 unsigned long rawSteering;
@@ -20,8 +28,10 @@ int throttleOut;
 int steeringOut;
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *drivingMotor = AFMS.getMotor(1);
+
+Servo steering;
+#define steeringServoPin 9
 
 //************Helper functions******************
 void readThrottle() {
@@ -34,13 +44,16 @@ void readSteering() {
 
 void mapThrottle() { //Converts raw throttle data to output
   //For week 1, return in range 0 to 100
-  throttleOut = rawThrottle;
+  if (rawThrottle < (rawThrottleMin + rawThrottleDeadZone)) rawThrottle = rawThrottleMin;
+  else if (rawThrottle > rawThrottleMax) rawThrottle = rawThrottleMax;
+
+  throttleOut = (rawThrottle - rawThrottleMin) * (throttleOutMax - throttleOutMin) / (rawThrottleMax - rawThrottleMin);
 }
 
 void mapSteering() { //Converts raw steering data to output
-  //For Week 1, return range -30 to 30
+  //For Week 1, return range -50 to 50
   //- values turn left, + values turn right
-  steeringOut = rawSteering;
+  steeringOut = 0;
 }
 
 //************Debugging*****************8
@@ -48,13 +61,13 @@ void mapSteering() { //Converts raw steering data to output
 //*************Setup and main loop**************
 void setup() {
   pinMode(throttlePin, INPUT);
-  pinMode(steeringPin, INPUT);
   
   Serial.begin(9600);
 
   AFMS.begin();
-  leftMotor->setSpeed(throttleOut);
-  rightMotor->setSpeed(throttleOut);
+  drivingMotor->setSpeed(throttleOut);
+
+  steering.attach(steeringServoPin);
 
   delay(1000);
 }
@@ -62,12 +75,12 @@ void setup() {
 void loop() {
   readThrottle();
   readSteering();
-  char buff[64];
-  sprintf(buff, "Throttle: %d Steering: %d", rawThrottle, rawSteering);
+  char buff[32];
+  sprintf(buff, "Pin reading: %d", rawThrottle);
   Serial.println(buff);
 
-  //leftMotor->setSpeed(throttleOut - steeringOut);
-  //rightMotor->setSpeed(throttleOut + steeringOut);
-
+  drivingMotor->setSpeed(throttleOut);
+  //steering.write(steeringOut);
+  
   delay(500);
 }
