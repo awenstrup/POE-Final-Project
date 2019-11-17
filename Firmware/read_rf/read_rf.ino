41cda36d2a97c002d2e04197aaf212575921d426
Alex Wenstrup <awenstrup@olin.edu>
2019-11-17 12:26:09 -0500

    Got variable throttle output working

--- Firmware/read_rf/read_rf.ino
+++ Firmware/read_rf/read_rf.ino
@@ -2,54 +2,53 @@
 #include <Servo.h>
 
 //*************Define constants and global variables***************
+
+//***********Define pins***************
 #define throttlePin 8 //PB0; PCINT0; column 1 on reciever side; left stick up/down
 #define steeringPin 6 //PD6, PCINT22; column 0 on reciever; right stick left/right
 
+#define steeringServoPin 9
+#define frontMotorPin 10
+#define rearMotorPin 3 //set to 11 before running
+
+//****************Raw input values*********************
+uint32_t rawThrottle = 0; //length of pulses
+uint32_t rawSteering = 0;
+
 //Min and max pulse lengths from reciever, used for mapping
-#define rawThrottleMin 1054
-#define rawThrottleMax 1888
+#define rawThrottleMin 1000
+#define rawThrottleMax 1988
 #define rawThrottleDeadZone 80
 
-#define rawSteeringMin 1052
-#define rawSteeringMax 1886
+#define rawSteeringMin 994
+#define rawSteeringMax 1987
 
+//******************Mapping values**************
+#define mapLow 0
+#define mapHigh 255
+
+int mappedThrottle = 0; //always maps 0-255
+int mappedSteering = 128; //always maps 0-255
+
+//**************Output values**********************************
 //Min and max values to write to DC motors through motor shield
-#define throttleOutMin -255
+#define throttleOutMin 0
 #define throttleOutMax 255
 
 //Min and max values to write to steering servo through motor shield
 #define steeringOutMin 50
 #define steeringOutMax 110
 
-unsigned long timer = 0;
-unsigned long lastThrotInt = 0;
-unsigned long lastSteerInt = 0;
+int throttleOut = 0; //output
+int steeringOut = 80; //output
+
+//**************Other global definitions**************
+volatile int timer = 0;
 boolean go = false;
-
-unsigned long rawThrottle = 0;
-unsigned long rawSteering = 0;
-
-//Stores the current state of the throttle and steering pins
-boolean throtPinState = false;
-boolean steerPinState = false;
-
-//Set to true when a pin's state is changed
-boolean throttleUpdate = false;
-boolean steeringUpdate = false;
-
-//Stores the time at which the pin went high
-unsigned long steerStartTime = 0;
-unsigned long throtStartTime = 0;
-
-int throttleOut = 0;
-int steeringOut = 80;
 
 Servo steering;
 Servo frontMotor;
 Servo rearMotor;
-#define steeringServoPin 9
-#define frontMotorPin 10
-#define rearMotorPin 3 //set to 11 before running
 
 //************Setup*****************
 void initTimer() { //addapted from https://www.instructables.com/id/Arduino-Timer-Interrupts/
@@ -63,96 +62,56 @@ void initTimer() { //addapted from https://www.instructables.com/id/Arduino-Time
   TIMSK2 |= (1 << OCIE2A);
 }
 
-void initPCINT() {
-  //Enables interupts on shutdown sense pins
-  PCICR |= 0b00000101; //enables PCINTs 16-23, 0-7
-  PCMSK0 |= 0b00000001; //enables PCINT0
-  PCMSK2 |= 0b01000000; //enables PCINT22
-}
-
-
 //*************Interupts*************
 ISR(TIMER2_COMPA_vect) {
     timer++;
-    if (true) go = true;
-}
-
-ISR(PCINT0_vect) {
-  if (micros() - lastThrotInt > 300) {
-    lastThrotInt = micros();
-    boolean t1 = digitalRead(throttlePin);
-    if (throttleUpdate) {
-      if (t1) throtStartTime = micros();
-      else rawThrottle = micros() - throtStartTime;
-  
-      throttleUpdate = false;
+    if (timer == 10) {
+      timer = 0;
+      go = true;
+      //digitalWrite(13, !digitalRead(13));
     }
-    //digitalWrite(13, !digitalRead(13));
-  }
 }
-
-ISR(PCINT2_vect) {
-  boolean t1 = digitalRead(steeringPin);
-  steeringUpdate = true;
-  steerPinState = t1;
-  steerStartTime = timer;
-  //digitalWrite(13, !digitalRead(13));
-}
-
-
 
 //************Helper functions******************
 void readThrottle() { 
-  //rudimentary implimentation used pulseIn arduino function
-  //in future, either feed PWM though RC filter and analogRead, or use interupts
-  //to read PWM more efficiently
   rawThrottle = pulseIn(throttlePin, HIGH);
-
-  //throttleUpdate = true;
 }
 
 void readSteering() {
-  //rudimentary implimentation used pulseIn arduino function
-  //in future, either feed PWM though RC filter and analogRead, or use interupts
-  //to read PWM more efficiently
   rawSteering = pulseIn(steeringPin, HIGH);
-
-  /*
-  if (steeringUpdate) {
-    if (steerPinState) {
-      steerStartTime = timer;
-    }
-    else {
-      rawSteering = timer - steerStartTime;
-    }
-    steeringUpdate = false;
-  }
-  */
 }
 
-void mapThrottle() { //Converts raw throttle data to output
-  //For week 1, return in range 0 to 255
+void mapThrottle() { //Converts raw throttle data to 0-255
+  /*With reverse
   int rawThrottleMidpoint = (rawThrottleMax + rawThrottleMin) / 2;
-
   if (abs(rawThrottle - rawThrottleMidpoint) < rawThrottleDeadZone) rawThrottle = rawThrottleMidpoint;
   else if (rawThrottle < rawThrottleMin) rawThrottle = rawThrottleMin;
   else if (rawThrottle > rawThrottleMax) rawThrottle = rawThrottleMax;
+  */
 
-  throttleOut = throttleOutMin + (rawThrottle - rawThrottleMin) * (throttleOutMax - throttleOutMin) / (rawThrottleMax - rawThrottleMin);
+  /*Without reverse*/
+  if (rawThrottle < rawThrottleMin + rawThrottleDeadZone) rawThrottle = rawThrottleMin;
+  else if (rawThrottle > rawThrottleMax) rawThrottle = rawThrottleMax;
+  mappedThrottle = mapLow + (rawThrottle - rawThrottleMin) * (mapHigh - mapLow) / (rawThrottleMax - rawThrottleMin);
 }
 
-void mapSteering() { //Converts raw steering data to output
-  //For week 1, return between 50 and 110
+void mapSteering() { //Converts raw steering data to 0-255
   if (rawSteering < rawSteeringMin) rawSteering = rawSteeringMin;
   else if (rawSteering > rawSteeringMax) rawSteering = rawSteeringMax;
 
-  steeringOut = steeringOutMin + (rawSteering - rawSteeringMin) * (steeringOutMax - steeringOutMin) / (rawSteeringMax - rawSteeringMin);
+  mappedSteering = mapLow + (rawSteering - rawSteeringMin) * (mapHigh - mapLow) / (rawSteeringMax - rawSteeringMin);
+}
+
+void outputThrottle() {
+  int s = abs(128 - abs(128 - mappedSteering));
+  float steeringScaler = (0.5 + s/255.0); //scales throttle from 0.5 to 1x full throttle based on steering angle
+  throttleOut = (int) (mappedThrottle * steeringScaler);
 }
 
 //************Debugging*****************
 void printThrottle() {
   char buff[64];
-  sprintf(buff, "Raw throttle: %d", rawThrottle);
+  sprintf(buff, "Raw throttle: %d", throttleOut);
   Serial.println(buff);
 }
 
@@ -163,18 +122,16 @@ void setup() {
   pinMode(steeringPin, INPUT);
 
   //Set pin 13 as debugging LED
-  pinMode(9, OUTPUT);
   //pinMode(13, OUTPUT);
 
   //Open serial port
   Serial.begin(9600);
 
-  steering.attach(steeringPin);
+  steering.attach(steeringServoPin);
   frontMotor.attach(frontMotorPin);
   rearMotor.attach(rearMotorPin);
 
   initTimer();
-  initPCINT();
   sei();
 }
 
@@ -183,18 +140,21 @@ void loop() {
     go = false;
     //Read raw throttle and steering values from receiver
     readThrottle();
-    //readSteering();
+    readSteering();
 
-    //Map raw throttle and steering data to output values
-    //mapThrottle();
-    //mapSteering();
+    //Map raw throttle and steering data
+    mapThrottle();
+    mapSteering();
+
+    //Remap throttle and steering data to output values
+    outputThrottle();
   
     //Drive rear wheels
-    frontMotor.write(throttleOut);
-    rearMotor.write(throttleOut);
+    //frontMotor.write(throttleOut);
+    //rearMotor.write(throttleOut);
 
     //Update steering angle
-    steering.write(steeringOut);
+    //steering.write(steeringOut);
 
     printThrottle();
   }
